a659ab18ece0b07c56b41f4c522f4cbd6bd6640b
marco <marco.crucitti96@gmail.com>
2025-07-04 00:45:05 +0200

    aggiunta della data

--- src/components/LiveClock.jsx
+++ src/components/LiveClock.jsx
@@ -11,12 +11,21 @@ const LiveClock = () => {
   }, []);
 
   return (
-    <div className="fw-bold">
-      {now.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
+    <div className="text-end">
+      <div className="fw-bold">
+        {now.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </div>
+      <div className="small text-uppercase">
+        {now.toLocaleDateString("it-IT", {
+          weekday: "short",
+          day: "2-digit",
+          month: "2-digit",
+        })}
+      </div>
     </div>
   );
 };
